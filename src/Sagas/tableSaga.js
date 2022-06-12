51adc8e27dc06ca6cd8e5db422b66e917479b0b0
Pankaj-Guser <dixitpankajguser@gmail.com>
2022-06-12 09:26:03 +0530

    all apis are working

--- src/Sagas/tableSaga.js
+++ src/Sagas/tableSaga.js
@@ -1,8 +1,52 @@
 import { call, fork, put, takeLatest } from "redux-saga/effects";
 import { tableActions } from "../actions/tableActions";
 import tableAPI from "../utils/tableAPI";
+import axios from "axios";
 import TableData from "../helpers/tableDataConverter";
+const callAPI = async ({ url, method, data }) => {
+  console.log("methode get called", url)
+  return await axios({
+    url,
+    method,
+    data
+  });
+};
+export function* addDataSaga(addRowData) {
+  try {
+    yield call(callAPI({url:'http://localhost:8000/api/v1/tenants/5bc8222c-974b-4113-9c59-0cbf5c157ead/knowledge_basis',
+    method:'POST',
+    data:addRowData}));
+    let result = yield call(() => tableAPI.fetchData());
+    result = TableData(result);
+    yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
+  } catch (e) {
+  console.log("update data fail")
 
+    yield put({
+      type: tableActions.ADD_SINGLE_ROW_DATA_ERROR,
+      payload: "API server is down",
+    });
+  }
+}
+export function* updateDataSaga(bodyData) {
+  try {
+    yield call(callAPI({url:'http://localhost:8000/api/v1/tenants/5bc8222c-974b-4113-9c59-0cbf5c157ead/knowledge_basis/'+bodyData.selectedKey,
+    method:'PUT',
+    data:bodyData.updatedCellData}));
+    // yield call(() => tableAPI.updateSingleRowData, bodyData.selectedKey, bodyData.updatedCellData);
+    let result = yield call(() => tableAPI.fetchData());
+    result = TableData(result);
+    yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
+  } catch (e) {
+  console.log("update data fail")
+
+    yield put({
+      
+      type: tableActions.UPDATE_SINGLE_ROW_DATA_ERROR,
+      payload: "API server is down",
+    });
+  }
+}
 export function* fetchDataSaga() {
   try {
     let result = yield call(() => tableAPI.fetchData());
@@ -29,42 +73,7 @@ export function* deleteDataSaga(selectedRow) {
   }
 }
 
-export function* updateDataSaga(bodyData) {
-  try {
 
-    yield call(() => tableAPI.updateSingleRowData, bodyData.selectedKey, bodyData.updatedCellData);
-    let result = yield call(() => tableAPI.fetchData());
-    result = TableData(result);
-    yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
-  } catch (e) {
-  console.log("update data fail")
-
-    yield put({
-      
-      type: tableActions.UPDATE_SINGLE_ROW_DATA_ERROR,
-      payload: "API server is down",
-    });
-  }
-}
-
-export function* addDataSaga(addRowData) {
-  try {
-    yield call(
-      tableAPI.addSingleRowData,
-      addRowData.createdCellData
-    );
-    let result = yield call(() => tableAPI.fetchData());
-    result = TableData(result);
-    yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
-  } catch (e) {
-  console.log("update data fail")
-
-    yield put({
-      type: tableActions.ADD_SINGLE_ROW_DATA_ERROR,
-      payload: "API server is down",
-    });
-  }
-}
 
 export function* fetchTableDataAll() {
   yield takeLatest(tableActions.FETCH_DATA_SAGA, fetchDataSaga);
