dfdee6f9d8fbdb882593ad7b6b0f1750e56664f4
Pankaj-Guser <dixitpankajguser@gmail.com>
2022-06-12 10:09:55 +0530

    Update changes

--- src/Sagas/tableSaga.js
+++ src/Sagas/tableSaga.js
@@ -14,13 +14,14 @@ const callAPI = async ({ url, method, data }) => {
 };
 export function* addDataSaga(addRowData) {
   try {
-    const response = yield call(callAPI({url:'http://localhost:8000/api/v1/tenants/5bc8222c-974b-4113-9c59-0cbf5c157ead/knowledge_basis',
+    const response = yield call( () => callAPI({url:'http://localhost:8000/api/v1/tenants/5bc8222c-974b-4113-9c59-0cbf5c157ead/knowledge_basis',
     method:'POST',
     data:addRowData.createdCellData}));
-    console.log("add api response",response)
-    let result = yield call(() => tableAPI.fetchData());
-    result = TableData(result);
-    yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
+    if(response.success === true) {
+      let result = yield call(() => tableAPI.fetchData());
+      result = TableData(result);
+      yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
+    }
   } catch (e) {
 
     yield put({
@@ -31,15 +32,15 @@ export function* addDataSaga(addRowData) {
 }
 export function* updateDataSaga(bodyData) {
   try {
-    const response = yield call(callAPI({url:'http://localhost:8000/api/v1/tenants/5bc8222c-974b-4113-9c59-0cbf5c157ead/knowledge_basis/'+bodyData.selectedKey,
+    const response = yield call(() => callAPI({url:'http://localhost:8000/api/v1/tenants/5bc8222c-974b-4113-9c59-0cbf5c157ead/knowledge_basis/'+bodyData.selectedKey,
     method:'PUT',
     data:bodyData.updatedCellData}));
-    console.log("update api response",response)
-
-    // yield call(() => tableAPI.updateSingleRowData, bodyData.selectedKey, bodyData.updatedCellData);
-    let result = yield call(() => tableAPI.fetchData());
-    result = TableData(result);
-    yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
+    if(response.success === true) {
+      // yield call(() => tableAPI.updateSingleRowData, bodyData.selectedKey, bodyData.updatedCellData);
+      let result = yield call(() => tableAPI.fetchData());
+      result = TableData(result);
+      yield put({ type: tableActions.GET_TABLE_DATA_SUCCESS, payload: result });
+    }
   } catch (e) {
 
     yield put({
